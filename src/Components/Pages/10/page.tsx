2dce2f5afc6692e6a3ed8aa17645bf484d0f11f4
Tartos <sword41@mail.ru>
2025-03-13 11:48:38 +0300

    feat: add cards arrivles - add html/css

--- src/Components/Pages/10/page.tsx
+++ src/Components/Pages/10/page.tsx
@@ -5,6 +5,7 @@
 import TasksBackBtn from "../../Shared/tasksBackBtn";
 
 import React, { useState } from "react";
+import { Rate } from "antd";
 import {
   CircleUserRound,
   Search,
@@ -19,11 +20,17 @@ import { menuItems } from "./data";
 // ~ assets
 import __logo from "../../Shared/page10/images/__logo.svg";
 import __bannerImage from "../../Shared/page10/images/__bannerImage1.jpg";
+// партнеры
 import __part1 from "../../Shared/page10/images/__part1.svg";
 import __part2 from "../../Shared/page10/images/__part2.svg";
 import __part3 from "../../Shared/page10/images/__part3.svg";
 import __part4 from "../../Shared/page10/images/__part4.svg";
 import __part5 from "../../Shared/page10/images/__part5.svg";
+// карточки поступлений
+import __ar1 from "../../Shared/page10/images/__ar1.jpg";
+import __ar2 from "../../Shared/page10/images/__ar2.jpg";
+import __ar3 from "../../Shared/page10/images/__ar3.jpg";
+import __ar4 from "../../Shared/page10/images/__ar4.jpg";
 
 // ~ styles
 import "./style.scss";
@@ -201,6 +208,63 @@ export default function Page(props: any) {
       <div className="test10__section">
         <div className="test10--title">
           <h2>NEW ARRIVALS</h2>
+        </div>
+      </div>
+
+      {/* cards arrivles  */}
+      <div className="test10__section">
+        <div className="test10--wrapper">
+          <div className="test10--cardsAr">
+            <div className="__card">
+              <img src={__ar1} alt="" />
+              <div className="__info">
+                <h4>T-SHIRT WITH TAPE DETAILS</h4>
+                <Rate />
+                <div className="__price">
+                  <div className="__current">$ 120</div>
+                  {/* <div className="__old">$ 160</div>
+                <div className="__percent">30%</div> */}
+                </div>
+              </div>
+            </div>
+            <div className="__card">
+              <img src={__ar2} alt="" />
+              <div className="__info">
+                <h4>SKINNY FIT JEANS</h4>
+                <Rate />
+                <div className="__price">
+                  <div className="__current">$ 240</div>
+                  <div className="__old">$ 260</div>
+                  <div className="__percent">20%</div>
+                </div>
+              </div>
+            </div>
+            <div className="__card">
+              <img src={__ar3} alt="" />
+              <div className="__info">
+                <h4>CHECKERED SHIRT</h4>
+                <Rate />
+                <div className="__price">
+                  <div className="__current">$ 180</div>
+                  {/* <div className="__old">$ 160</div>
+                <div className="__percent">30%</div> */}
+                </div>
+              </div>
+            </div>
+            <div className="__card">
+              <img src={__ar4} alt="" />
+              <div className="__info">
+                <h4>SLEEVE STRIPED T-SHIRT</h4>
+                <Rate />
+                <div className="__price">
+                  <div className="__current">$ 130</div>
+                  <div className="__old">$ 160</div>
+                  <div className="__percent">30%</div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <button className="__viewAll">View all</button>
         </div>
       </div>
     </div>
