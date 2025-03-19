1ff2fc03361059ab209b3db71976e3ec19216451
Victor <adflasher@mail.ru>
2025-03-19 21:19:04 +0300

    feat: adding tests - fix  problems with jest libs

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
+  moduleNameMapper: {
+    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
+  },
+  setupFilesAfterEnv: ["<rootDir>/setupTests.ts"],
+  transform: {
+    "^.+\\.(ts|tsx)$": "ts-jest",
+  },
+  testMatch: ["**/?(*.)+(spec|test).[jt]s?(x)"],
+};
