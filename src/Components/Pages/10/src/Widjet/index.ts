1e256bf481d2b288d20d2b8c7d8f7325720e4e66
Victor <adflasher@mail.ru>
2025-03-24 15:23:56 +0300

    refactor: page10 - fix bag slider height

--- src/Components/Pages/10/src/Widjet/index.ts
+++ src/Components/Pages/10/src/Widjet/index.ts
@@ -4,4 +4,3 @@ export { default as PartnersRow } from "./PartnersRow/__components/PartnersRow";
 export { default as TitleRow } from "./TitleRow/__components/TitleRow";
 export { default as ProductsList } from "./ProductsList/ProductsList";
 export { default as FashionGrid } from "./FashionGrid/FashionGrid";
-export { default as ReviewSlider } from "./ReviewSlider/ReviewSlider";
