71854174748f5f33699cc27022f97c5a025674f3
Victor <adflasher@mail.ru>
2025-03-26 20:48:13 +0300

    refactor: page10 - add slides

--- src/Components/Feature/ViewCodeStructure/ViewCodeStructure.tsx
+++ src/Components/Feature/ViewCodeStructure/ViewCodeStructure.tsx
@@ -10,54 +10,11 @@ import StructurePanel from "./__components/StructurePanel/StructurePanel";
 // ~ redux
 import { useAppSelector } from "../../Pages/10/src/Redux/hooks";
 
-// ~ assets
-import _1 from "../../Pages/10/src/Shared/assets/__ar1.jpg";
-import _2 from "../../Pages/10/src/Shared/assets/__ar2.jpg";
-import _3 from "../../Pages/10/src/Shared/assets/__ar3.jpg";
-import _4 from "../../Pages/10/src/Shared/assets/__ar4.jpg";
 // #endregion ~ __IMPORT__
 
-const __data = [
-  {
-    id: 0,
-    name: "Main",
-    description:
-      "Архитектура сайта построена по передовому принципу Feature-Sliced Design (FSD), что обеспечивает максимальную гибкость, масштабируемость и удобство разработки. Благодаря единому контекстному пространству навигация по сайту интуйтивно понятна: вы легко найдёте нужные категории, акции и новинки, а адаптивный дизайн гарантирует комфортный просмотр с любого устройства.",
-    content: [
-      {
-        id: 0,
-        title: "",
-        description: "- Реализация - React + Typescript + css modules (sccs)",
-        slides: [],
-      },
-      { id: 1, title: "", description: "- Стейт менеджер - Redux", slides: [] },
-      {
-        id: 2,
-        title: "",
-        description: "- Структура проекта - FSD",
-        slides: [],
-      },
-      {
-        id: 3,
-        title: "",
-        description: "- Дополнительные модули - Webpack",
-        slides: [],
-      },
-      {
-        id: 4,
-        title: "",
-        description: "- Тестирование - JEST, React Testing Lib",
-        slides: [],
-      },
-    ],
-  },
-  { id: 1, name: "Структура", description: "d1", content: [] },
-  { id: 2, name: "Redux", description: "d2", content: [] },
-  { id: 3, name: "Тестирование", description: "d3", content: [] },
-  { id: 4, name: "Webpack", description: "d4", content: [] },
-  { id: 5, name: "Custom slider", description: "d5", content: [] },
-  { id: 6, name: "Webpack testid cleaner", description: "d6", content: [] },
-];
+// ! data
+import { structerData } from "../../Pages/10/src/Data/structerData";
+const __data = structerData;
 
 // #region ~ __COMPONENT__
 export default function ViewCodeStructure() {
---
+++ src/Components/Pages/10/src/Data/structerData.tsx
@@ -0,0 +1,73 @@
+// Структура
+import __1 from "../../../../Shared/image/page10/__str_1.jpg";
+import __2 from "../../../../Shared/image/page10/__str_2.jpg";
+import __3 from "../../../../Shared/image/page10/__str_3.jpg";
+import __4 from "../../../../Shared/image/page10/__str_4.jpg";
+import __5 from "../../../../Shared/image/page10/__str_5.jpg";
+
+export const structerData = [
+  {
+    id: 0,
+    name: "Main",
+    description:
+      "Архитектура сайта построена по передовому принципу Feature-Sliced Design (FSD), что обеспечивает максимальную гибкость, масштабируемость и удобство разработки. Благодаря единому контекстному пространству навигация по сайту интуйтивно понятна: вы легко найдёте нужные категории, акции и новинки, а адаптивный дизайн гарантирует комфортный просмотр с любого устройства.",
+    content: [
+      {
+        id: 0,
+        title: "",
+        description: "- Реализация - React + Typescript + css modules (sccs)",
+        slides: [],
+      },
+      { id: 1, title: "", description: "- Стейт менеджер - Redux", slides: [] },
+      {
+        id: 2,
+        title: "",
+        description: "- Структура проекта - FSD",
+        slides: [],
+      },
+      {
+        id: 3,
+        title: "",
+        description: "- Дополнительные модули - Webpack",
+        slides: [],
+      },
+      {
+        id: 4,
+        title: "",
+        description: "- Тестирование - JEST, React Testing Lib",
+        slides: [],
+      },
+    ],
+  },
+  {
+    id: 1,
+    name: "Структура",
+    description:
+      "FSD-архитектура была осознанно выбрана для этого проекта — она идеально соответствует его внутренней логике и структуре. Такой подход обеспечивает плавное, почти органичное масштабирование: система может расти и усложняться, сохраняя при этом кристальную ясность организации. Это как строить город, где каждое новое здание естественно вписывается в продуманную планировку улиц, а не превращается в хаотичный лабиринт.",
+    content: [
+      {
+        id: 0,
+        title: "",
+        description: "Общая структура папок",
+        slides: [__1, __2, __3, __4, __5],
+      },
+    ],
+  },
+  {
+    id: 2,
+    name: "Redux",
+    description: "d2",
+    content: [
+      {
+        id: 0,
+        title: "",
+        description: "",
+        slides: [],
+      },
+    ],
+  },
+  { id: 3, name: "Тестирование", description: "d3", content: [] },
+  { id: 4, name: "Webpack", description: "d4", content: [] },
+  { id: 5, name: "Custom slider", description: "d5", content: [] },
+  { id: 6, name: "Webpack testid cleaner", description: "d6", content: [] },
+];
