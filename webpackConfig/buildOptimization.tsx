e956a829126313b158773034f60d4ed23d282788
Victor <adflasher@mail.ru>
2025-03-29 22:34:03 +0300

    refactor: add js mini plugin

--- webpackConfig/buildOptimization.tsx
+++ webpackConfig/buildOptimization.tsx
@@ -1,12 +1,25 @@
 import { Configuration } from "webpack";
 import CssMinimizerPlugin from "css-minimizer-webpack-plugin";
+import TerserPlugin from "terser-webpack-plugin";
 
 import { int_BuildOptions } from "./types/types";
 
 export function buildOptimization(
   options: int_BuildOptions
 ): Configuration["optimization"] {
+  const isProd = options.mode === "production";
+
+  // Общие
+  const plugins: (CssMinimizerPlugin | TerserPlugin)[] = [];
+
+  if (isProd) {
+    plugins.push(
+      new CssMinimizerPlugin(),
+      new TerserPlugin() // Добавляем минификатор JS
+    );
+  }
   return {
-    minimizer: [new CssMinimizerPlugin()],
+    minimize: isProd,
+    minimizer: plugins,
   };
 }
